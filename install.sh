1792ce9ca52403518eb4ba5e8583827a13f08679
Aaditya1978 <aadityasinghal1978@gmail.com>
2021-02-09 15:57:51 +0530

    Added script for installing required packages

---
+++ install.sh
@@ -0,0 +1,7 @@
+# !/bin/bash
+sudo apt-get install -y python3-pip
+pip3 install Flask
+pip3 install marshmallow-sqlalchemy
+pip3 install flask-marshmallow
+pip3 install flask-sqlalchemy
+pip3 install pytz
